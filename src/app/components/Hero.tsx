30aad457f7281e4b4803ec03b5b2d8ae1c9038e4
Apollo Daniel <developer.apollo.mail@gmail.com>
2025-10-22 17:43:13 -0300

    feat: add background to hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,23 +7,23 @@ export function Hero({ id }: { id?: string }) {
   return (
     <div
       id={id}
-      className="text-neutral-900 p-[160px] flex justify-between  w-full relative min-h-[680px] overflow-hidden"
+      className="relative flex min-h-[680px] w-full justify-between overflow-hidden p-[160px] text-neutral-900"
     >
-      <div className=" ">
-        <h1 className="text-neutral-900 w-[620px] text-3xl  font-medium">
+      <div>
+        <h1 className="w-[620px] text-3xl font-medium text-neutral-900">
           Acelere as <span className="italic">Vendas</span> do seu
           Empreendimento. Ofereça{" "}
           <span className="font-bold">Personalização Sem Complicações.</span>
         </h1>
-        {/* <Image
+        <Image
           alt="hero-image"
-          src="/hero-image1.png"
+          src="/paisagem-urbana-background.jpg"
           width={1006}
           height={1006}
-          className="absolute left-0 -bottom-30"
-        /> */}
+          className="absolute bottom-0 left-0"
+        />
       </div>
-      <div className="flex flex-col gap-4 justify-end items-end text-end text-sm">
+      <div className="flex flex-col items-end justify-end gap-4 text-end text-sm">
         <p className="w-[305px]">
           A Plana AdHoc é a plataforma white-label que automatiza o processo de
           personalização de unidades. Aumente seu VGV, reduza custos
