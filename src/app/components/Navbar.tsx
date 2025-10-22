77802e07f8cc154926c4a610383e05f8b25821df
Apollo Daniel <developer.apollo.mail@gmail.com>
2025-10-22 12:14:28 -0300

    feat: add demo link to navbar test button

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Button } from "./Button";
+import Link from "next/link";
 
 export function Navbar() {
   return (
-    <nav className="  w-full px-4 flex items-center justify-center md:px-8 lg:px-12 py-4 md:py-6 z-10 top-0 sticky  bg-[#F9F5EC]">
-      <div className="flex items-center w-full max-w-[1280px] justify-between">
+    <nav className="sticky top-0 z-10 flex w-full items-center justify-center bg-[#F9F5EC] px-4 py-4 md:px-8 md:py-6 lg:px-12">
+      <div className="flex w-full max-w-[1280px] items-center justify-between">
         <Image width={122} height={38} src="/logo.png" alt="Plana AdHoc Logo" />
-        <Button title="Teste Agora!" className="hidden md:block" />
+        <Link href="/demo" passHref>
+          <Button title="Teste Agora!" className="hidden md:block" />
+        </Link>
       </div>
     </nav>
   );
